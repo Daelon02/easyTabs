0b8f3a28cd5a1103575dbcd4eb277e8d38957247
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-12-29 01:13:57 +0200

    Test

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelectorAll('.info-header'),
+        info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
@@ -21,7 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    info.forEach.call(info,function(el){
         el.addEventListener('click', function(event) {
             let target = event.target;
             if (target && target.classList.contains('info-header-tab')) {
@@ -33,6 +32,5 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
                 }
             }
-        })
-    });
+        });
 });
